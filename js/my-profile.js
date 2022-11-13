34124c8527daf1f3c2735f46eedcb39f01519db7
mattiasalvez <mattialvez@gmail.com>
2022-11-13 15:25:52 -0300

    Entrega final 7
    final

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,13 +1,16 @@
 let mail = obtenerInfo();
 document.getElementById("emailpr").value = mail;
 
-    //Validaciones
     const validacionesPr = document.getElementsByClassName("valiProfile");
     const nameProfile = document.getElementById("namepr");
     const surnameProfile = document.getElementById("surnamepr");
     const mailProfile = document.getElementById("emailpr");
     const save = document.getElementById("btnSave");
+    const name2Profile = document.getElementById("name2pr");
+    const surname2Profile = document.getElementById("surname2pr");
+    const phoneProfile = document.getElementById("phone");
 
+    //Validaciones
     function validationProfile(){
         for (const element of validacionesPr) {
             if (!element.checkValidity()) {
@@ -29,6 +32,16 @@ document.getElementById("emailpr").value = mail;
             if (mailProfile.value){
                 localStorage.setItem("mailing", mailProfile.value);
             } else false;
+            if (name2pr.value){
+                localStorage.setItem("nombre2", name2Profile.value);
+            } else false;
+            if (surname2Profile.value){
+                localStorage.setItem("apellido2", surname2Profile.value);
+            } else false;
+            if (phoneProfile.value){
+                localStorage.setItem("telefono", phoneProfile.value);
+            } else false;    
+            alerts();       
         }
 
        save.addEventListener("click", function(e){
@@ -36,6 +49,15 @@ document.getElementById("emailpr").value = mail;
         complete();
        })
 
+       //Mantengo la información en pantalla
        nameProfile.value = localStorage.getItem("nombre");
        surnameProfile.value = localStorage.getItem("apellido");
-   
+       name2Profile.value = localStorage.getItem("nombre2");
+       surname2Profile.value = localStorage.getItem("apellido2");
+       phoneProfile.value = localStorage.getItem("telefono");
+
+       function alerts(){
+        if (nameProfile.value && surnameProfile.value && mailProfile.value){
+            alert("Datos guardados con éxito");
+        } else false;
+       }
